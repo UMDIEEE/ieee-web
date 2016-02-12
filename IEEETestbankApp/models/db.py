64c5a005baad5e6e60c8980d6102c611de33a096
Albert Huang <alberthdev@users.noreply.github.com>
2016-02-12 09:57:26 -0500

    Ensure that DB existence check only runs once

--- IEEETestbankApp/models/db.py
+++ IEEETestbankApp/models/db.py
@@ -4,9 +4,16 @@ import sqlalchemy_utils
 from IEEETestbankApp import app
 import IEEETestbankApp.models
 
+global run_once
+run_once = False
+
 def initDatabase():
-    if not sqlalchemy_utils.functions.database_exists(app.config['SQLALCHEMY_DATABASE_URI']):
-        sqlalchemy_utils.functions.create_database(app.config['SQLALCHEMY_DATABASE_URI'])
+    global run_once
+    
+    if not run_once:
+        run_once = True
+        if not sqlalchemy_utils.functions.database_exists(app.config['SQLALCHEMY_DATABASE_URI']):
+            sqlalchemy_utils.functions.create_database(app.config['SQLALCHEMY_DATABASE_URI'])
     
     print("Initializing database!")
     db.create_all()
