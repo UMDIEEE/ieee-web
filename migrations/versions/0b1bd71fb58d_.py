25f169c8b3a80d435c892554757c6a4dd7748513
Albert Huang <alberthdev@users.noreply.github.com>
2016-02-22 07:41:02 -0500

    Increase value size

---
+++ migrations/versions/0b1bd71fb58d_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 0b1bd71fb58d
+Revises: None
+Create Date: 2016-02-22 07:39:26.255154
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '0b1bd71fb58d'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/5ddd839de57c_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 5ddd839de57c
+Revises: 0b1bd71fb58d
+Create Date: 2016-02-22 07:40:21.142754
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '5ddd839de57c'
+down_revision = '0b1bd71fb58d'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
