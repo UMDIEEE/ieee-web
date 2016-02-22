3bf66a14fd9bd9a4b8459ae25a24a4d403f53952
Albert Huang <alberthdev@users.noreply.github.com>
2016-02-22 08:21:59 -0500

    Change People to Plus API

--- IEEETestbankApp/views/admin/testbank.py
+++ IEEETestbankApp/views/admin/testbank.py
@@ -74,8 +74,8 @@ def admin_testbank_settings():
                 testbank_settings_form = form)
             
         http_auth = credentials.authorize(httplib2.Http())
-        people_service = discovery.build('people', 'v1', http_auth)
-        linked_acct_info = people_service.people().get(user_id='me').execute()
+        people_service = discovery.build('plus', 'v1', http_auth)
+        linked_acct_info = people_service.people().get(userId='me').execute()
         linked_acct_info_name = linked_acct_info.get('names')[0].get("displayName")
         linked_acct_info_email = linked_acct_info.get('emailAddresses')[0].get("value")
         config_gdrive_user = [ linked_acct_info_name, linked_acct_info_email ]
